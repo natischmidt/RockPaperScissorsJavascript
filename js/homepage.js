2e35c845e281fb6890d85cdacdfd61524fa67717
OBorgstrom <111687752+OBorgstrom@users.noreply.github.com>
2023-02-15 20:14:53 +0100

    Hello? wip med alexsis sandros

--- js/homepage.js
+++ js/homepage.js
@@ -4,3 +4,12 @@ let popup = document.getElementById("myPopup");
 function openRulesPopup() {
     popup.classList.toggle("show");
 }
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
