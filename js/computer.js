886c3e49dd7a8ae796797c35fd28b40f025da1a1
nati schmidt <nati.schmidt97@outlook.de>
2023-02-22 10:17:37 +0100

    wip

--- js/computer.js
+++ js/computer.js
@@ -12,3 +12,10 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+function openBody(){
+    document.getElementById("bodyid").style.visibility= 'visible' ;
+    document.getElementById("start-btn").style.visibility = 'hidden' ;
+    document.getElementById("popup").style.visibility = 'hidden' ;
+
+}
