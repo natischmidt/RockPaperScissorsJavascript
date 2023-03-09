44db21131394de1fc8ce08dd28c406690753e733
nati schmidt <nati.schmidt97@outlook.de>
2023-03-09 13:27:32 +0100

    wip

--- js/pvp.js
+++ js/pvp.js
@@ -8,6 +8,36 @@ let moves = 0;
 // function refreshGame() {
 //     setInterval(refreshGameInfo, 3000);
 // }
+function winnerchickendinner(playeroneMove, playertwoMove){
+    if(playeroneMove === playertwoMove){
+        console.log('Draw');
+        return;
+    }
+    if(playeroneMove === 'Rock'){
+        if(playertwoMove === 'Scissor'){
+            totalPlayerOne += 1;
+        }
+        if(playertwoMove === 'Paper'){
+            totalPlayerTwo += 1;
+        }
+    }
+    if(playeroneMove === 'Scissor'){
+        if(playertwoMove === 'Rock'){
+            totalPlayerTwo += 1;
+        }
+        if(playertwoMove === 'Paper'){
+            totalPlayerOne += 1;
+        }
+    }
+    if(playeroneMove === 'Paper'){
+        if(playertwoMove === 'Rock'){
+            totalPlayerOne += 1;
+        }
+        if(playertwoMove === 'Scissor'){
+            totalPlayerTwo += 1;
+        }
+    }
+}
 
 function refreshGameInfo() {
     rpsApi.gameInfo(rpsApi.getGameId())
